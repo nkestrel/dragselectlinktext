5f7eb721ca1feef7c3ecdd7edadea57437417491
kestrel <kestrel@vmail.me>
2016-12-10 18:02:12 +0800

    Copyright date update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* 
  * Drag-Select Link Text for Firefox
- * Copyright (C) 2014 Kestrel
+ * Copyright (C) 2014-2016 Kestrel
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
