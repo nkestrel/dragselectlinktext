5d923c41ff46a3ef5c9d43aa6f1cf22683e2705d
kestrel <kestrel@vmail.me>
2017-10-24 19:32:30 +0800

    Remove drag threshold storage reset on update.

--- background.js
+++
@@ -1,7 +0,0 @@
-
-browser.runtime.onInstalled.addListener((details) => {
-  if (details.reason === "update") {
-    browser.storage.local.set({dragThresholdX: defaultOptions.dragThresholdX,
-                               dragThresholdY: defaultOptions.dragThresholdY});
-  }
-});
