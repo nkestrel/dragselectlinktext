b40254ef4c7d6173d9c2759741ba2604a7f3d8b3
kestrel <kestrel@vmail.me>
2018-01-20 19:14:49 +0800

    Update copyright notice year for 2018.

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 /*
  * Drag-Select Link Text
  * Firefox Web Extension
- * Copyright (C) 2014-2017 Kestrel
+ * Copyright (C) 2014-2018 Kestrel
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
